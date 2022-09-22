284413ef27d9b392b88f1ebe1cc10f50280ef2c4
ManonSella <Manon@manons-macbook-air.home>
2022-09-22 17:49:34 +0200

    Fixed an error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
         <main>
           <Dictionary />
         </main>
